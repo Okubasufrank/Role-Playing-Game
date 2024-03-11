a4fec879929c3a403ec2bdf157f185bed38f6e64
Frankie <franklineokubasu51@gmail.com>
2024-03-11 12:33:36 +0300

    added text propertyon the object

--- script.js
+++ script.js
@@ -56,7 +56,8 @@ function buyWeapon(){}
 
 const locations = [
     {name : "town square", 
-    "button text":["Go to store", "Go to cave", "Fight dragon"]
-    "button functions" : [goStore, goCave, fightDragon]
+    "button text":["Go to store", "Go to cave", "Fight dragon"],
+    "button functions" : [goStore, goCave, fightDragon],
+    text : "You are in the town square. You see a sign that says \"Store\"."
 }
 ];
