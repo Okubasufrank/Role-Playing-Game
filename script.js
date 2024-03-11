e7d87320e10fcdd38a7acac2d5d6287b623c72ef
Frankie <franklineokubasu51@gmail.com>
2024-03-11 12:28:49 +0300

    added variables on the property button functions

--- script.js
+++ script.js
@@ -55,5 +55,8 @@ function buyHealth(){}
 function buyWeapon(){}
 
 const locations = [
-    {name : "town square"}
+    {name : "town square", 
+    "button text":["Go to store", "Go to cave", "Fight dragon"]
+    "button functions" : [goStore, goCave, fightDragon]
+}
 ];
