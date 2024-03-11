1953f396562d4d932ef713f111fb210b4fa3b15f
Frankie <franklineokubasu51@gmail.com>
2024-03-11 11:37:59 +0300

    added properties to function goTown

--- script.js
+++ script.js
@@ -23,15 +23,24 @@ button1.onclick = goStore;
 button2.onclick = goCave;
 button3.onclick = fightDragon;
 
+function goTown(){
+    button1.innerText = "Go to store";
+    button2.innerText = "Go to cave";
+    button3.innerText = "Fight dragon";
+    button1.onclick = goStore;
+    button2.onclick = goCave;
+    button3.onclick = fightDragon;
+    text.innerText = "You are in the town squre. You see a sign that says store.";
+}
 
 function goStore(){
     button1.innerText = "Buy 10 Health (10 gold)";
     button2.innerText = "Buy weapon (30 gold)";
     button3.innerText = "Go to town square";
-
     button1.onclick = buyHealth;
     button2.onclick = buyWeapon;
     button3.onclick = goTown;
+    text.innerText = "You enter the store.";
 }
 
 function goCave(){
@@ -41,3 +50,6 @@ function goCave(){
 function fightDragon(){
     console.log("Fighting dragon.");
 }
+
+function buyHealth(){}
+function buyWeapon(){}
