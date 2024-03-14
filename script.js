c03f10a8efad50f69c7edfee7cba67967d58495e
Frankie <franklineokubasu51@gmail.com>
2024-03-14 13:40:51 +0300

    added two other functions

--- script.js
+++ script.js
@@ -55,6 +55,9 @@ function goStore(){
     update(locations[1]);
 }
 
+function fightSlime(){};
+
+function fightBeast(){};
 /*button1.innerText = "Buy 10 Health (10 gold)";
 button2.innerText = "Buy weapon (30 gold)";
 button3.innerText = "Go to town square";
