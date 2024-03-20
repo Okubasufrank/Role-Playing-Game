8571343dc52351077e09b5f5aba58f72181810b0
Frankie <franklineokubasu51@gmail.com>
2024-03-21 00:24:23 +0300

    updating innerText text using +=

--- script.js
+++ script.js
@@ -113,6 +113,7 @@ function buyWeapon(){
         let newWeapon = weapons[currentWeapon];
         text.innerText = "You now have a " + newWeapon +".";
         inventory.push(newWeapon);
+        text.innerText += " In your inventory you have: ";
     }
 }
 
