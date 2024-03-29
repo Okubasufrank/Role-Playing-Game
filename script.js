c494522849f6b6059f6d2ce32992dc61998b3d69
Frankie <franklineokubasu51@gmail.com>
2024-03-29 15:56:16 +0300

    condition not to sell the only weapon

--- script.js
+++ script.js
@@ -125,12 +125,16 @@ function buyWeapon(){
 }
 }  
 
-function sellWeapon(inventory.length > 1){
+function sellWeapon(){
+if(inventory.length > 1){
     gold += 15;
     goldText.innerText = gold;
     let currentWeapon = inventory.shift();
     text.innerText = "You sold a" + currentWeapon + ".";
     text.innerText += " In your inventory you have: " + inventory + ".";
+    }else {
+        text.innerText = "Dont sell your only weapon!"
+    }
 }
 
 
