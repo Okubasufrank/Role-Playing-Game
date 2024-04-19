c354dc402d23a0d1b5a82622d93fdc9c9be6c48a
Frankie <franklineokubasu51@gmail.com>
2024-04-19 18:39:49 +0300

    substracting health from monsters fighting level prpty

--- script.js
+++ script.js
@@ -171,6 +171,7 @@ function goFight(){
 
 function attack(){
     text.innerText = 'The ' + monsters[fighting].name + 'attack.';
-    text.innerText += " You attack it with your " + weapons[currentWeapon].name + "."; 
+    text.innerText += " You attack it with your " + weapons[currentWeapon].name + ".";
+    health -= monsters[fighting].level; 
 }
 function dodge(){}
