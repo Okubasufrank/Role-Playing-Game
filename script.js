406573fdc291f3ee8c4ae027dbcf0741148183da
Frankie <franklineokubasu51@gmail.com>
2024-04-27 23:55:57 +0300

    setting health to equal helath and passing the value of the monster as an argument

--- script.js
+++ script.js
@@ -191,7 +191,7 @@ function goFight(){
 function attack(){
     text.innerText = 'The ' + monsters[fighting].name + 'attack.';
     text.innerText += " You attack it with your " + weapons[currentWeapon].name + ".";
-    health -= monsters[fighting].level; 
+    health -= getMonsterAttackValue(monsters[fighting].level);; 
     monsterHealth -= weapons[currentWeapon].power + Math.floor(Math.random() * xp) + 1;
     healthText.innerText = health;
     monsterHealthText.innerText = monsterHealth;
