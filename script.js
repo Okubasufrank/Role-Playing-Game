77a8169890eff3f5245a66283dda75cb76479977
Frankie <franklineokubasu51@gmail.com>
2024-04-27 23:10:53 +0300

    Created function winGame and called update function with locations array as the argument and obect 6

--- script.js
+++ script.js
@@ -206,6 +206,10 @@ function lose(){
     update(locations[5])
 }
 
+function winGame(){
+    update(locations[6])
+}
+
 function defeatMonster(){
     gold += Math.floor(monsters[fighting].level * 6.7)
     xp += monsters[fighting].level;
