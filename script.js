9b49754178cbe4a593438cc6432900b4f71380d0
Frankie <franklineokubasu51@gmail.com>
2024-04-28 12:59:22 +0300

    using decreament operator

--- script.js
+++ script.js
@@ -209,7 +209,8 @@ function attack(){
         }
     }
     if(Math.random() <= .1){
-        
+        text.innerText += " Your " + inventory.pop() + " breaks."
+        currentWeapon -- ;
     }
 }
 
