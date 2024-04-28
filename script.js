49be67c02cfd48ee8325ecedb6d52c4d4dd72f20
Frankie <franklineokubasu51@gmail.com>
2024-04-28 13:16:17 +0300

    using AND operator to add the second condition

--- script.js
+++ script.js
@@ -208,9 +208,9 @@ function attack(){
             defeatMonster()
         }
     }
-    if(Math.random() <= .1){
+    if(Math.random() <= .1 && inventory.length !== 1){
         text.innerText += " Your " + inventory.pop() + " breaks."
-        currentWeapon -- ;
+        currentWeapon-- ;
     }
 }
 
