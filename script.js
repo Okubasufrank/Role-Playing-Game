38050f3f6cc295cef5f473e0fd7eb6a7c42393b1
Frankie <franklineokubasu51@gmail.com>
2024-04-28 11:24:28 +0300

    adding new function with level as a param

--- script.js
+++ script.js
@@ -205,6 +205,11 @@ function attack(){
         }
     }
 }
+
+function getMonsterAttackValue(level){
+
+}
+
 function dodge(){
     text.innerText = "You dodge the attack from the " + monsters[fighting].name;
 }
