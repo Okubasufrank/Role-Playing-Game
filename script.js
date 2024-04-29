9450abe7908aaf5f894b1936da78a03f2d124b7a
Frankie <franklineokubasu51@gmail.com>
2024-04-29 11:02:04 +0300

    concatinating with param geuss

--- script.js
+++ script.js
@@ -268,8 +268,9 @@ function easterEgg(){
 function pick(guess){
     const numbers = [];
     while (numbers.length < 10){
-    numbers.push(Math.floor(Math.random() * 11))
+    numbers.push(Math.floor(Math.random() * 11  ))
     }
+    text.innerText =  "You picked " + guess +". Here are the random numbers:"
 }
 
 function pickTwo (){
